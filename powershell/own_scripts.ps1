13a4b4bf759e453a47336596399dbafea381a041
quanzhai <quanzhai@gmail.com>
2020-05-12 11:49:30 +0200

    add some powershell scripts

---
+++ powershell/own_scripts.ps1
@@ -0,0 +1,4 @@
+# grant permissions
+GrantPermission.ps1 -ParamSqlInstance SCB-PP-SE-AG01 -ParamLogin SCB\BF.PP.T520020 -ParamDBRole write  -Param Database eArchive
+
+###########################################################################################
