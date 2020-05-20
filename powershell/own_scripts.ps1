9009273e3486904d77a16d89402b6a4320e4abd4
quanzhai <quanzhai@gmail.com>
2020-05-20 16:08:51 +0200

    wrong syntax

--- powershell/own_scripts.ps1
+++ powershell/own_scripts.ps1
@@ -1,4 +1,7 @@
 # grant permissions
-GrantPermission.ps1 -ParamSqlInstance SCB-PP-SE-AG01 -ParamLogin SCB\BF.PP.T520020 -ParamDBRole write  -Param Database eArchive
+.\GrantPermission.ps1 -ParamSqlInstance SCB-PP-SE-AG01 -ParamLogin SCB\BF.PP.T520020 -ParamDBRole write  -ParamDatabase db1
+.\GrantPermission.ps1 -ParamSqlInstance SCB-PP-SE-AG01 -ParamLogin SCB\BF.PP.T520020 -ParamDBRole admin  -ParamDatabase db2
+.\GrantPermission.ps1 -ParamSqlInstance SCB-PP-SE-AG01 -ParamLogin SCB\BF.PP.T520020 -ParamDBRole read  -ParamDatabase db3
+.\GrantPermission.ps1 -ParamSqlInstance SCB-PP-SE-AG01 -ParamLogin SCB\BF.PP.T520020 -ParamDBRole execute  -ParamDatabase db4
 
 ###########################################################################################
