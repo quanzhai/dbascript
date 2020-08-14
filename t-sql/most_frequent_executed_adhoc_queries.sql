86d7f4271e3bedbcd07e5f2314901223a4686227
quanzhai <quanzhai@gmail.com>
2020-08-14 09:08:07 +0200

    most frequent ad hoc query and dup query i cache

--- t-sql/most_frequent_executed_adhoc_queries.sql
+++ t-sql/most_frequent_executed_adhoc_queries.sql
@@ -1,13 +1,34 @@
-
-SELECT [name], s.database_id,
-COUNT(l.database_id) AS 'VLF Count',
-SUM(vlf_size_mb) AS 'VLF Size (MB)',
-SUM(CAST(vlf_active AS INT)) AS 'Active VLF',
-SUM(vlf_active*vlf_size_mb) AS 'Active VLF Size (MB)',
-COUNT(l.database_id)-SUM(CAST(vlf_active AS INT)) AS 'In-active VLF',
-SUM(vlf_size_mb)-SUM(vlf_active*vlf_size_mb) AS 'In-active VLF Size (MB)'
-FROM sys.databases s
-CROSS APPLY sys.dm_db_log_info(s.database_id) l
-GROUP BY [name], s.database_id
-ORDER BY 'VLF Count' DESC
-GO
+--Most frequent executed queries Adhoc and Procedure
+;WITH qh AS
+(
+    SELECT TOP (25) query_hash, COUNT(*) AS COUNT
+    FROM sys.dm_exec_query_stats
+    GROUP BY query_hash
+    ORDER BY COUNT(*) DESC
+),
+qs AS
+(
+    SELECT obj = COALESCE(ps.object_id,   fs.object_id,   ts.object_id),
+            db = COALESCE(ps.database_id, fs.database_id, ts.database_id),
+            qs.query_hash, qs.query_plan_hash, qs.execution_count,
+            qs.sql_handle, qs.plan_handle
+    FROM sys.dm_exec_query_stats AS qs
+    INNER JOIN qh ON qs.query_hash = qh.query_hash
+    LEFT OUTER JOIN sys.dm_exec_procedure_stats AS [ps]
+      ON [qs].[sql_handle] = [ps].[sql_handle]
+    LEFT OUTER JOIN sys.dm_exec_function_stats AS [fs]
+      ON [qs].[sql_handle] = [fs].[sql_handle]
+    LEFT OUTER JOIN sys.dm_exec_trigger_stats AS [ts]
+      ON [qs].[sql_handle] = [ts].[sql_handle]
+)
+SELECT TOP (50)
+  OBJECT_NAME(qs.obj, qs.db), 
+  query_hash,
+  query_plan_hash,
+  SUM([qs].[execution_count]) AS [ExecutionCount],
+  MAX([st].[text]) AS [QueryText]
+  FROM qs 
+  CROSS APPLY sys.dm_exec_sql_text ([qs].[sql_handle]) AS [st]
+  CROSS APPLY sys.dm_exec_query_plan ([qs].[plan_handle]) AS [qp]
+  GROUP BY qs.obj, qs.db, qs.query_hash, qs.query_plan_hash
+  ORDER BY ExecutionCount DESC;
